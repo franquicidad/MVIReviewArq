0c69863e2848135b7c84c654452d1cde332c7922
felipefranco <franquicidad@gmail.com>
2021-03-27 22:23:43 -0500

    initial commit

---
+++ app/src/main/java/com/example/mvireview/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvireview.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvireview.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
